31b24708d1c78ad8aed2a450227cbeb6e842f9be
Dmitry <dmtrys@inbox.lv>
2015-04-04 19:21:34 +0300

    reload appear

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
+function check(){
+	if( $("#cube_03").css('display') == 'none'&&$("#cube_02").css('display') == 'none'&&$("#cube_01").css('display') == 'none'){
+		//alert('none');
+		$("#reload").css( "display", "inline-block" );
+	}
+	else{
+		//alert('block');
+	}
+}
+
 $( document ).ready(function() {
 	
 	$("div").click(function() {
 		$("#"+this.id).not("#main_wrapper, #inner_wrapper, #main_title, #reload_wrapper").fadeOut( "slow", function() {
+		check();
 		});
-		// $("div").not("#main_wrapper, #inner_wrapper").remove("#"+this.id);
 	});
 	
 });
 
 function show(){
+	$("#reload").css( "display", "none" );
 	$("#cube_01, #cube_02, #cube_03").css( "display", "inline-block" );
 }

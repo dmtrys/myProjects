33db1ca7f3830d4cae7c675cb225262eb30137ca
Dmitry <dmtrys@inbox.lv>
2015-04-04 17:05:28 +0300

    reload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $( document ).ready(function() {
 	
 	$("div").click(function() {
-		$("#"+this.id).not("#main_wrapper, #inner_wrapper").fadeOut( "slow", function() {
+		$("#"+this.id).not("#main_wrapper, #inner_wrapper, #main_title, #reload_wrapper").fadeOut( "slow", function() {
 		});
+		// $("div").not("#main_wrapper, #inner_wrapper").remove("#"+this.id);
 	});
 	
 });
+
+function show(){
+	$("#cube_01, #cube_02, #cube_03").css( "display", "inline-block" );
+}
